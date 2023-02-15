d201b1c6ebd442dd1a3f68358eaf0991b95465dd
Mandeep <1mandeep2021@gmail.com>
2023-02-15 17:57:35 +0530

    get api working

--- server/src/server.js
+++ server/src/server.js
@@ -2,14 +2,21 @@ const http = require('http');
 
 const app = require('./app');
 
+const { loadPlanetData, path_check } = require('./models/planets.model');
+
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+async function startServer() {
+    
+    // await path_check(); 
+    await loadPlanetData(); 
+    
+}
+
 server.listen(PORT, ()=> {
     console.log(`Listing on port ${PORT}`);
 });
+startServer();
 
-
-
-console.log(PORT);
